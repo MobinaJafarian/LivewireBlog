1dc127411183a7aa98f746a9d62ab0832fd2897e
mobinajafarian <nimobina99@gmail.com>
2023-05-13 14:44:01 +0330

    add articles table

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,3 +1,34 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
+<div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-4 lg:-mx-6">
+      <div class="inline-block min-w-full py-2 sm:px-4 lg:px-6">
+        <div class="overflow-hidden">
+          <table class="min-w-full text-left text-sm font-light">
+            <thead class="border-b font-medium dark:border-neutral-500">
+              <tr>
+                <th scope="col" class="px-6 py-4">#</th>
+                <th scope="col" class="px-6 py-4">Title</th>
+                <th scope="col" class="px-6 py-4">Description</th>
+                <th scope="col" class="px-6 py-4">Link</th>
+                <th scope="col" class="px-6 py-4">Settings</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($articles as $key => $article)  
+              <tr
+                class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                <td class="whitespace-nowrap px-6 py-4 font-medium">{{ $key += 1}}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $article->title }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $article->description }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $article->link }}</td>
+                <td class="whitespace-nowrap px-6 py-4">
+                <button type="submit" class="inline-block rounded bg-info px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" data-te-ripple-init data-te-ripple-color="dark">
+                    Edit</button>
+                </td>
+              </tr>
+               @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
