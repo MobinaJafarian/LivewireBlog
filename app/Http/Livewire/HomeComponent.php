b8f73ff77739389f55b195b6b24a1733371b66f8
mobinajafarian <nimobina99@gmail.com>
2023-05-13 14:44:39 +0330

    update

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -19,15 +19,4 @@ class HomeComponent extends Component
         return view('livewire.home-component', compact('articles'));
     }
 
-    // public function store()
-    // {
-    //     $post = new Post();
-    //     $post->title = $this->title;
-    //     $imageName = Carbon::now()->timestamp . '.' . $this->image->extension();
-    //     $this->image->storeAz('img', $imageName);
-    //     $post->image = $imageName;
-    //     $post->save();
-
-    //     session()->flash('success', 'post saved');
-    // }
 }
