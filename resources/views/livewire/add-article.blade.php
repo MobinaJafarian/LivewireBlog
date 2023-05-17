424271bed033c75017af29650eab88402a042eb8
mobinajafarian <nimobina99@gmail.com>
2023-05-17 22:32:26 +0330

    add @error

--- resources/views/livewire/add-article.blade.php
+++ resources/views/livewire/add-article.blade.php
@@ -14,6 +14,9 @@
         wire:model="title"
         wire:keyup="generateLink()"
         placeholder="Title" />
+        @error('title')
+            {{$message}}
+        @enderror
       <label
         for="exampleInput7"
         class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -30,6 +33,9 @@
         rows="3"
         wire:model="description"
         placeholder="Message"></textarea>
+        @error('description')
+            {{$message}}
+        @enderror
       <label
         for="exampleFormControlTextarea13"
         class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
