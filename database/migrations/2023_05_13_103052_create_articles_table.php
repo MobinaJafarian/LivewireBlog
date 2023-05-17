a9fadbbcd3d065d6b04e47707cc0dccc37dc107d
mobinajafarian <nimobina99@gmail.com>
2023-05-17 23:03:54 +0330

    add image to articles table

--- database/migrations/2023_05_13_103052_create_articles_table.php
+++ database/migrations/2023_05_13_103052_create_articles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('link');
             $table->string('description');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
