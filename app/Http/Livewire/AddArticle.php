a3ea61760ad32bf8c5f90c71c75732de63c6e1b3
mobinajafarian <nimobina99@gmail.com>
2023-05-17 23:03:31 +0330

    add file upload

--- app/Http/Livewire/AddArticle.php
+++ app/Http/Livewire/AddArticle.php
@@ -5,16 +5,21 @@ namespace App\Http\Livewire;
 use App\Models\Article;
 use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AddArticle extends Component
 {
+    use WithFileUploads;
+
     public $title;
     public $description;
     public $link;
+    public $image;
 
     protected $rules = [
         'title' => 'required',
         'description' => 'required',
+        'image' => 'image|max:1024',
     ];
 
     public function render()
@@ -30,12 +35,15 @@ class AddArticle extends Component
     public function addArticle()
     {
         $this->validate();
-        $article  = new Article();
+        $article = new Article();
         $article->title = $this->title;
         $article->description = $this->description;
         $article->link = $this->link;
+        $imageName = time() . '.' . $this->image->extension();
+        $this->image->storeAs('articles', $imageName);
+        $article->image = $imageName;
         $article->save();
         session()->flash('success', 'Article saved successfully');
     }
-     
+
 }
