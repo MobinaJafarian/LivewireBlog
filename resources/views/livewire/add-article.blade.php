da0dc0d78e93d8b002a64e807ce03b6b2b58dd0a
mobinajafarian <nimobina99@gmail.com>
2023-05-17 23:04:15 +0330

    add ImageUpload

--- resources/views/livewire/add-article.blade.php
+++ resources/views/livewire/add-article.blade.php
@@ -43,7 +43,32 @@
       </label>
     </div>
 
-    
+    <!--Image upload-->
+    <div class="relative mb-6" data-te-input-wrapper-init>
+      <input
+        type="file"
+        class="peer block min-h-[auto] w-full rounded border-0 border-solid border-neutral-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"        id="exampleInput7"
+        wire:model="image"
+        wire:keyup="generateLink()"
+        placeholder="image" />
+        @error('image')
+            {{$message}}
+        @enderror
+      <label
+        for="exampleInput7"
+        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+        >Image
+      </label>
+    </div>
+
+    <div class="relative mb-6" data-te-input-wrapper-init>
+      @if ($image)
+          <img src=" {{$image->temporaryUrl() }}" alt="">
+      @endif
+    </div>
+
+
+
     <!--Submit button-->
     <button
       type="submit"
